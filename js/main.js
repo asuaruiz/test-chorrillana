575411d1651ae8e02924a499eaae63d10d48f36b
asuaruiz <asuaruiz@gmail.com>
2016-11-07 13:38:08 -0300

    funcionando boton jquery

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/*function anadirBtn(){
+	var btnJs = document.createElement("button");
+	var texto = document.createTextNode("Boton JS");
+	btnJs.appendChild(texto);
+//	btnJs.type = "button";
+	btnJs.id = "btn-js";
+	btnJs.setAttribute("href","#");
+	btnJs.setAttribute("id","myList")
+	btnJs.classList.add("btn","btn-primary","file-menu");
+	document.getElementById("padreBtn").appendChild(btnJs);
+	//document.getElementsByTagName("li")[0].appendChild(btnJs);
+}
+anadirBtn();
+*/
+
+/*function anadirHijo() {
+    var ulnode = document.createElement("ul");
+    ulnode.setAttribute("id","ulnode");
+    var node = document.createElement("li");
+    node.setAttribute("id","node");
+    var refer = document.createElement("a");
+    var textnode = document.createTextNode("Hijo Js 1");
+    node.appendChild(refer);
+    refer.appendChild(textnode);
+    ulnode.appendChild(node);
+    document.getElementById("myList").appendChild(ulnode);
+}
+anadirHijo();*/
+
