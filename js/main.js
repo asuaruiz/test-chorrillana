eb16e0b4aa5ced13c821bb334ef304dcd5f44947
asuaruiz <asuaruiz@gmail.com>
2016-11-07 15:37:51 -0300

    test listo

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
-/*function anadirBtn(){
-	var btnJs = document.createElement("button");
-	var texto = document.createTextNode("Boton JS");
-	btnJs.appendChild(texto);
-//	btnJs.type = "button";
-	btnJs.id = "btn-js";
-	btnJs.setAttribute("href","#");
-	btnJs.setAttribute("id","myList")
-	btnJs.classList.add("btn","btn-primary","file-menu");
-	document.getElementById("padreBtn").appendChild(btnJs);
-	//document.getElementsByTagName("li")[0].appendChild(btnJs);
+function anadirBtn(){
+    var papaBtn = document.createElement("div");
+    var btnJs = document.createElement("button");
+    var texto = document.createTextNode("Hover Css");
+    papaBtn.classList.add("dropdown");
+    papaBtn.appendChild(btnJs);
+    btnJs.appendChild(texto);
+    btnJs.id = "btn-js";
+    btnJs.setAttribute("id","myList");
+    btnJs.classList.add("btn","btn-primary","dropbtn");
+    document.getElementById("padreBtn").appendChild(papaBtn);
 }
 anadirBtn();
-*/
 
-/*function anadirHijo() {
+
+function anadirHijo() {
     var ulnode = document.createElement("ul");
-    ulnode.setAttribute("id","ulnode");
+    ulnode.classList.add("dropdown-content")
     var node = document.createElement("li");
-    node.setAttribute("id","node");
-    var refer = document.createElement("a");
+    node.classList.add("dropdown-menu-li");
     var textnode = document.createTextNode("Hijo Js 1");
-    node.appendChild(refer);
-    refer.appendChild(textnode);
+    node.appendChild(textnode);
     ulnode.appendChild(node);
     document.getElementById("myList").appendChild(ulnode);
 }
-anadirHijo();*/
+anadirHijo();
+
+
+
 
